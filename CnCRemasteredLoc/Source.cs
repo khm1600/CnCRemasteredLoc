aeed6d07b547bfe6d70cfce964b2d0d88514994e
khm1600 <44062845+khm1600@users.noreply.github.com>
2024-09-15 16:40:00 +0800

    Add LocFileToCsv. Refactor.

--- CnCRemasteredLoc/Source.cs
+++ CnCRemasteredLoc/Source.cs
@@ -9,80 +9,101 @@ namespace CnCRemasteredLoc
     {
         public static string LocFileToJson(string path)
         {
-            using var s = File.OpenRead(path);
-
-            var count = ReadUInt32(s);
-
-            var entries = new Entry[count];
-
-            for (int i = 0; i < count; i++)
-            {
-                entries[i].magic = ReadUInt32(s);
-                entries[i].entryLength = ReadUInt32(s);
-                entries[i].entryTagLength = ReadUInt32(s);
-            }
-
-            for (int i = 0; i < count; i++)
-            {
-                var length = entries[i].entryLength;
-                var sb = new StringBuilder((int)length);
-                for (int j = 0; j < length; j++)
-                {
-                    var ch = ReadUInt16(s);
-                    _ = sb.Append((char)ch);
-                }
-                entries[i].entry = sb.ToString();
-            }
-
-            for (int i = 0; i < count; i++)
-            {
-                var length = entries[i].entryTagLength;
-                var sb = new StringBuilder((int)length);
-                for (int j = 0; j < length; j++)
-                {
-                    var ch = s.ReadByte();
-                    _ = sb.Append((char)ch);
-                }
-                entries[i].entryTag = sb.ToString();
-            }
+            var entries = LoadFile(path);
 
             var json = JsonSerializer.Serialize(entries);
 
             return json;
         }
 
-        static uint ReadUInt32(Stream s)
+        public static string LocFileToCsv(string path)
         {
-            uint result = 0;
+            var entries = LoadFile(path);
 
-            for (int i = 0; i < 4; i++)
+            var result = new StringBuilder();
+            result.AppendLine("\"entryTag\",\"entry\"");
+
+            foreach (var item in entries)
             {
-                var b = s.ReadByte();
-                if (b == -1)
-                {
-                    throw new EndOfStreamException();
-                }
-                result |= (uint)(b & 0xFF) << (i * 8);
+                result.AppendLine($"\"{item.entryTag}\",\"{item.entry}\"");
             }
-
-            return result;
+            return result.ToString();
         }
 
-        static ushort ReadUInt16(Stream s)
+        static Entry[] LoadFile(string path)
         {
-            ushort result = 0;
+            using var s = File.OpenRead(path);
 
-            for (int i = 0; i < 2; i++)
+            var count = ReadUInt32(s);
+
+            var result = new Entry[count];
+
+            for (int i = 0; i < count; i++)
             {
-                var b = s.ReadByte();
-                if (b == -1)
+                result[i].magic = ReadUInt32(s);
+                result[i].entryLength = ReadUInt32(s);
+                result[i].entryTagLength = ReadUInt32(s);
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                var length = result[i].entryLength;
+                var sb = new StringBuilder((int)length);
+                for (int j = 0; j < length; j++)
                 {
-                    throw new EndOfStreamException();
+                    var ch = ReadUInt16(s);
+                    _ = sb.Append((char)ch);
                 }
-                result |= (ushort)((b & 0xFF) << (i * 8));
+                result[i].entry = sb.ToString();
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                var length = result[i].entryTagLength;
+                var sb = new StringBuilder((int)length);
+                for (int j = 0; j < length; j++)
+                {
+                    var ch = s.ReadByte();
+                    _ = sb.Append((char)ch);
+                }
+                result[i].entryTag = sb.ToString();
             }
 
             return result;
+
+            static uint ReadUInt32(Stream s)
+            {
+                uint result = 0;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    var b = s.ReadByte();
+                    if (b == -1)
+                    {
+                        throw new EndOfStreamException();
+                    }
+                    result |= (uint)(b & 0xFF) << (i * 8);
+                }
+
+                return result;
+            }
+
+            static ushort ReadUInt16(Stream s)
+            {
+                ushort result = 0;
+
+                for (int i = 0; i < 2; i++)
+                {
+                    var b = s.ReadByte();
+                    if (b == -1)
+                    {
+                        throw new EndOfStreamException();
+                    }
+                    result |= (ushort)((b & 0xFF) << (i * 8));
+                }
+
+                return result;
+            }
         }
     }
 
